fa5ba2fe1f5a756f40ee1b81b429e560266277e4
Brock Tillotson - G15296 <brock.tillotson@microchip.com>
2017-06-16 09:00:04 -0700

    working merge function now done

--- merge_sort.rb
+++ merge_sort.rb
@@ -4,15 +4,17 @@
 
 def merge(list1, list2)
   result = []
-  until list1.length == 0 && list2.length == 0
+  until list1.empty? && list2.empty?
     if list1.length == 0
       list2.each do |x|
         result.push(x)
       end
-    elsif list2.length == 0
+      list2.clear
+    elsif list2.empty?
       list1.each do |x|
         result.push(x)
       end
+      list1.clear
     else
       if list1[0] < list2[0]
         result.push(list1[0])
@@ -21,8 +23,7 @@ def merge(list1, list2)
         result.push(list2[0])
         list2.delete_at(0)
       end
-    end
-    
+    end  
   end
   result
 end
